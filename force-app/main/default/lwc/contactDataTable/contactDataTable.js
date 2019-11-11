b7254cfc161ae487ebf50e594050f2541fd7c71c
Taylor Kingsbury <taylorkingsbury@gmail.com>
2019-11-10 20:01:35 -0500

    adding view modal

--- force-app/main/default/lwc/contactDataTable/contactDataTable.js
+++ force-app/main/default/lwc/contactDataTable/contactDataTable.js
@@ -1,5 +1,8 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import getContacts from '@salesforce/apex/ContactsController.getContacts';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+import {refreshApex} from '@salesforce/apex';
 
 const columns = [
     { label: 'FirstName', fieldName: 'FirstName'},
@@ -20,8 +23,40 @@ export default class ContactDataTable extends LightningElement {
     //reactive
     @api recordId;
     @track columns = columns;
+    @track bShowModal = false;
 
     @wire(getContacts,{ accountId: '$recordId'})
     contacts;
+
+
+    handleRowAction(event){
+        const row = event.detail.row;
+        this.record = row;
+        this.bShowModal = true;
+    }
+
+    // closing modal box
+    closeModal() {
+        this.bShowModal = false;
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        this.template.querySelector('lightning-record-edit-form').submit(event.detail.fields);
+
+
+        this.bShowModal = false;
+
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Success!!',
+            message: event.detail.fields.FirstName + ' '+ event.detail.fields.LastName +' Contact updated Successfully!!.',
+            variant: 'success'
+        }),);
+    }
+
+    handleSuccess() {
+        return refreshApex(this.refreshTable);
+    }
     
 }
